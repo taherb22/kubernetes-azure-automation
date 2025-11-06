3b2069a2c125c3aca68140b63eb7d6808bf451d9
ghaitho20 <ghaith.benammar709@gmail.com>
2025-11-06 17:49:34 +0000

    feat ansible

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "tfstate-rg"
-    storage_account_name  = "tfstatek8s21718"
-    container_name        = "tfstate"
-    key                   = "terraform.tfstate"
-  }
-}
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name   = "tfstate-rg"
+#     storage_account_name  = "tfstatek8s21718"
+#     container_name        = "tfstate"
+#     key                   = "terraform.tfstate"
+#   }
+# }
